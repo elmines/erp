e39050bab8453249c29f788300a765be50062dd7
Ethan Mines <elmines325@gmail.com>
2018-11-09 23:36:30 -0600

    Restructure repo

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 import tensorflow as tf
-tf.set_random_seed(0)
 import numpy as np
 import json
 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+tf.set_random_seed(0)
+np.random.seed(0)
 
 def readJson(path):
 	with open(path, "r") as r:
@@ -45,54 +47,70 @@ def prepare(neg, pos):
 
 	return (inputArray, labelsArray)
 
-#def accuracy(predictions, labels):
+def partition(sequence: list, trainingPercent: float):
+	trainingIndex = int(len(sequence) * trainingPercent)
+	trainingSet = sequence[:trainingIndex]
+	testSet = sequence[trainingIndex:]
+	return (trainingSet, testSet)
 
 
+trainingPercent = 0.75
 paths = readJson("paths.json")	
-negTrain = filterPackets(readJson(paths["training"]["negative"]))
-posTrain = filterPackets(readJson(paths["training"]["positive"]))
-(inputTrain, labelsTrain) = prepare(negTrain, posTrain)
-numEpochs = 20
+neg = filterPackets(readJson(paths["negative"]))
+pos = filterPackets(readJson(paths["positive"]))
 
+(inputData, labelData) = prepare(neg, pos)
+#Shuffle
+randomIndices = np.random.permutation(len(inputData))
+inputData = inputData[randomIndices]
+labelData = labelData[randomIndices]
+
+(trainingInputs, testingInputs) = partition(inputData, trainingPercent)
+(trainingLabels, testingLabels) = partition(labelData, trainingPercent)
+
+numEpochs = 20
 #TRAINING
 #Graph
-inputs = tf.placeholder(tf.float32, (None, inputTrain.shape[-1]) )
+inputs = tf.placeholder(tf.float32, (None, trainingInputs.shape[-1]) )
 labels = tf.placeholder(tf.float32, None)
-logits = tf.squeeze(tf.layers.dense(inputs, 1))
-loss = tf.losses.sigmoid_cross_entropy(labels, logits)
+logit = tf.squeeze(tf.layers.dense(inputs, 1))
+loss = tf.losses.sigmoid_cross_entropy(labels, logit)
 optimizer = tf.train.AdamOptimizer()
 train_op = optimizer.minimize(loss)
 
 #TESTING
-negTest = filterPackets(readJson(paths["testing"]["negative"]))
-posTest = filterPackets(readJson(paths["testing"]["positive"]))
-(inputTest, labelsTest) = prepare(negTest, posTest)
 #Graph
-predictions = tf.cast(tf.round(tf.nn.sigmoid(logits)), tf.int32)
+predictions = tf.cast(tf.round(tf.nn.sigmoid(logit)), tf.int32)
 
-print("Training on {} samples for {} epochs".format(len(inputTrain), numEpochs))
-print("\t{} negative training cases, {} positive training cases".format(len(negTrain), len(posTrain)))
+numNegTraining = sum(trainingLabels == 1)
+numPosTraining = len(trainingLabels) - numNegTraining
+numNegTesting = sum(testingLabels == 1)
+numPosTesting = len(testingLabels) - numNegTesting
+
+print("Training on {} samples for {} epochs".format(len(trainingInputs), numEpochs))
+print("\t{} negative training cases, {} positive training cases".format(numNegTraining, numPosTraining))
+print("\t{} negative testing  cases, {} positive testing  cases".format(numNegTesting, numPosTesting))
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
 
 	bestAccuracy = -1
 	for i in range(numEpochs):
-		[lossValue, _] = sess.run([loss, train_op], {inputs: inputTrain, labels:labelsTrain})
-		print("Epoch {}: loss = {}".format(i, lossValue))
+		[lossValue, _] = sess.run([loss, train_op], {inputs: trainingInputs, labels:trainingLabels})
+		print("Epoch {}: loss = {}".format(i, lossValue), flush=True)
 
-		print("Testing on {} samples".format(len(inputTest)))
-		[predictionValues] = sess.run([predictions], {inputs: inputTest} )
-		accuracy = np.mean( labelsTest == predictionValues )
+		print("Testing on {} samples".format(len(testingInputs)))
+		[predictionValues] = sess.run([predictions], {inputs: testingInputs} )
+		accuracy = np.mean( testingLabels == predictionValues )
 		print("\t   accuracy =", accuracy)
 		print()
 		if accuracy >= bestAccuracy: bestAccuracy = accuracy
 		else:                        break
 
-print("LDA Model")
+print("LDA Model", flush=True)
 ldaModel = LinearDiscriminantAnalysis()
-ldaModel.fit(inputTrain, labelsTrain)
-ldaPredictions = ldaModel.predict(inputTest)
-ldaAccuracy = np.mean( labelsTest == ldaPredictions)
+ldaModel.fit(trainingInputs, trainingLabels)
+ldaPredictions = ldaModel.predict(testingInputs)
+ldaAccuracy = np.mean( testingLabels == ldaPredictions)
 print("accuracy =", ldaAccuracy)
 
