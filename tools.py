0b564c19d010806f4bdf539b53aff3cec57b844a
Ethan Mines <elmines325@gmail.com>
2019-01-15 19:52:24 -0600

    Refactor training for new EEG data pipeline

--- tools.py
+++ tools.py
@@ -1,15 +1,56 @@
 import numpy as np
 import json
+import sys
+
+def maxReading():
+	return 80
+
+
 def readJson(path):
 	with open(path, "r") as r:
 		jsonData = json.load(r)
 	return jsonData
-#Remove nulls, keep backs 
+
+def resample(sequence, length, display=False):
+	if len(sequence) < length:
+		if display: print("Padding by {} packets".format(length-len(sequence)))
+		return sequence + [sequence[-1]] * (length-len(sequence))
+	else:
+		if display: print("Truncating {} packets".format(len(sequence)-length))
+		return sequence[:length]
+
+def resampleBatch(sequences, length):
+	return np.array([resample(sequence, length) for sequence in sequences])
+
+def preprocess(jsonData, padLength=256):
+	processed = []
+	for epoch in jsonData:
+		if not any(dirty(packet["data"]) for packet in epoch) and len(epoch) > 0:
+			processed.append( resample(epoch, padLength) )
+
+	arrayed = [ [packet["data"] for packet in epoch] for epoch in processed ]
+	arrayed = np.array(arrayed)
+	arrayed = np.transpose(arrayed, (0, 2, 1))
+
+	return arrayed
+
+
+def dirty(sequence):
+	return any(sample < -maxReading() or maxReading() < sample for sample in sequence)
+
+def taper(data):
+	"""
+	Tapers values outside [-tools.maxReading(), tools.maxReading()]
+	"""
+	data[data > maxReading()] = maxReading()
+	data[data < -maxReading()] = -maxReading()
+
+#Remove nulls, keep backs
 def filterPackets(jsonData):
 	filtered = []
 	for packet in jsonData:
 		if packet is None: continue
-		allChannelsClean = True 
+		allChannelsClean = True
 		for channel in packet["data"]:
 			if any(sample < -80 or 80 < sample for sample in channel):
 				allChannelsClean = False
@@ -24,7 +65,7 @@ def prepare(neg, pos):
 
 	Returns:
 		(inputArray, labelsArray)
-		Both are np.ndarray's 
+		Both are np.ndarray's
 		inputArray  has shape [numNegPackets+numPosPackets, numChannels*numSamples]
 		labelsArray has shape [numNegPackets+numPosPackets]
 	"""
@@ -39,6 +80,35 @@ def partition(sequence: list, trainingPercent: float):
 	trainingSet = sequence[:trainingIndex]
 	testSet = sequence[trainingIndex:]
 	return (trainingSet, testSet)
+
+def json2numpy(jsonData):
+	"""
+
+	:returns: Tuple of the positive samples and negative samples
+	:rtype: (np.ndarray, np.ndarray)
+	"""
+	data = np.array( [ packet for packet in jsonData["data"] ] )
+
+	neg = []
+	pos = []
+	i = 0
+	while i < len(data):
+		packet_set = []
+		j = i
+		while j < len(data) and data[i][-1] and len(packet_set) < 256:
+			packet_set.append(data[i])
+			j += 1
+		if packet_set: pos.append(packet_set)
+		packet_set = []
+
+		while j < len(data) and not(data[i][-1]) and len(packet_set) < 256:
+			packet_set.append(data[i])
+			j += 1
+		if packet_set: neg.append(packet_set)
+		i = j
+
+	return (pos, neg)
+
 
 def accuracy(predictions, labels):
 	return np.mean( predictions == labels )
@@ -55,4 +125,3 @@ def f1(predictions, labels):
 	precisionVal = precision(predictions, labels)
 	recallVal = recall(predictions, labels)
 	return (precisionVal * recallVal) / (precisionVal + recallVal)
-
--- train.py
+++ train.py
@@ -2,6 +2,8 @@ import argparse
 import tensorflow as tf
 import numpy as np
 
+import sys
+
 from tools import *
 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
@@ -10,79 +12,92 @@ np.random.seed(0)
 
 
 def main(args):
-	trainingPercent = 0.75
-	paths = readJson("paths.json")	
-	neg = filterPackets(readJson(paths["negative"]))
-	pos = filterPackets(readJson(paths["positive"]))
-	
-	(inputData, labelData) = prepare(neg, pos)
-	#Shuffle
-	randomIndices = np.random.permutation(len(inputData))
-	inputData = inputData[randomIndices]
-	labelData = labelData[randomIndices]
-	(trainingInputs, testingInputs) = partition(inputData, trainingPercent)
-	(trainingLabels, testingLabels) = partition(labelData, trainingPercent)
+	padLength = 256
+	trainPercent = 0.75
+	rawDataset = readJson(args.data)
+	(rawPos, rawNeg) = json2numpy(rawDataset)
+	(pos, neg) = (resampleBatch(rawPos, padLength), resampleBatch(rawNeg, padLength))
+	taper(pos)
+	taper(neg)
 
-	numNegTraining = sum(trainingLabels == 1)
-	numPosTraining = len(trainingLabels) - numNegTraining
-	numNegTesting = sum(testingLabels == 1)
-	numPosTesting = len(testingLabels) - numNegTesting
-	print("Training on {} samples".format(len(trainingInputs)))
-	print("\t{} negative training cases, {} positive training cases".format(numNegTraining, numPosTraining))
-	print("\t{} negative testing  cases, {} positive testing  cases".format(numNegTesting, numPosTesting))
-	
+	data = np.concatenate( (pos, neg), axis=0 )
+	data = data.reshape( (data.shape[0], np.prod(data.shape[1:])) ) #Flatten latter axes
+	labels = np.array(len(pos) * [1] + len(neg) * [0])
+
+	#Shuffle
+	randomIndices = np.random.permutation(len(data))
+	data = data[randomIndices]
+	labels = labels[randomIndices]
+	(trainInputs, testInputs) = partition(data, trainPercent)
+	(trainLabels, testLabels) = partition(labels, trainPercent)
+
+	numNegTrain = sum(trainLabels == 1)
+	numPosTrain = len(trainLabels) - numNegTrain
+	numNegTest = sum(testLabels == 1)
+	numPosTest = len(testLabels) - numNegTest
+	print("Training on {} samples".format(len(trainInputs)))
+	print("\t{} negative training cases, {} positive training cases".format(numNegTrain, numPosTrain))
+	print("\t{} negative testing  cases, {} positive testing  cases".format(numNegTest, numPosTest))
+
 
 	if args.regression:
-		numEpochs = 20
-		#TRAINING
-		#Graph
-		inputs = tf.placeholder(tf.float32, (None, trainingInputs.shape[-1]) )
-		labels = tf.placeholder(tf.float32, None)
-		logit = tf.squeeze(tf.layers.dense(inputs, 1))
-		loss = tf.losses.sigmoid_cross_entropy(labels, logit)
-		optimizer = tf.train.AdamOptimizer()
-		train_op = optimizer.minimize(loss)
-		#TESTING
-		#Graph
-		predictions = tf.cast(tf.round(tf.nn.sigmoid(logit)), tf.int32)
-		
-		print("Training logistic regression model for {} epochs".format(numEpochs))
-		with tf.Session() as sess:
-			sess.run(tf.global_variables_initializer())
-			bestAccuracy = -1
-			for i in range(numEpochs):
-				[lossValue, _] = sess.run([loss, train_op], {inputs: trainingInputs, labels:trainingLabels})
-				print("Epoch {}: loss = {}".format(i, lossValue), flush=True)
-				print("Testing on {} samples".format(len(testingInputs)))
-
-				[predictionValues] = sess.run([predictions], {inputs: testingInputs} )
-				testAccuracy       = accuracy(testingLabels, predictionValues)
-				testPrecision      = precision(testingLabels, predictionValues)
-				testRecall         = recall(testingLabels, predictionValues)
-				testF1             = f1(testingLabels, predictionValues)
-				print("\t   accuracy={0}, precision={1}, recall={2}, f1={3}".format(
-					testAccuracy, testPrecision, testRecall, testF1))
-				print()
-				if testAccuracy >= bestAccuracy: bestAccuracy = testAccuracy
-				else:                        pass #break
-
+		print("Logistic Regression Model")
+		logRegression(trainInputs, trainLabels, testInputs, testLabels)
 	if args.lda:
 		print("LDA Model", flush=True)
-		ldaModel = LinearDiscriminantAnalysis()
-		ldaModel.fit(trainingInputs, trainingLabels)
-		ldaPredictions = ldaModel.predict(testingInputs)
-		ldaAccuracy    = accuracy(ldaPredictions, testingLabels) #np.mean( testingLabels == ldaPredictions)
-		ldaPrecision   = precision(ldaPredictions, testingLabels)
-		ldaRecall      = recall(ldaPredictions, testingLabels)
-		ldaF1          = f1(ldaPredictions, testingLabels)
-		print("\t   accuracy={0}, precision={1}, recall={2}, f1={3}".format(
-			ldaAccuracy, ldaPrecision, ldaRecall, ldaF1))
+		lda(trainInputs, trainLabels, testInputs, testLabels)
+
+
+def logRegression(trainInputs, trainLabels, testInputs, testLabels):
+	numEpochs = 20
+	#TRAINING
+	#Graph
+	inputs = tf.placeholder(tf.float32, (None, trainInputs.shape[-1]) )
+	labels = tf.placeholder(tf.float32, None)
+	logit = tf.squeeze(tf.layers.dense(inputs, 1))
+	loss = tf.losses.sigmoid_cross_entropy(labels, logit)
+	optimizer = tf.train.AdamOptimizer()
+	train_op = optimizer.minimize(loss)
+	#TESTING
+	#Graph
+	predictions = tf.cast(tf.round(tf.nn.sigmoid(logit)), tf.int32)
+
+	print("Training logistic regression model for {} epochs".format(numEpochs))
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+		bestAccuracy = -1
+		for i in range(numEpochs):
+			[lossValue, _] = sess.run([loss, train_op], {inputs: trainInputs, labels:trainLabels})
+			print("Epoch {}: loss = {}".format(i, lossValue), flush=True)
+			print("Testing on {} samples".format(len(testInputs)))
+
+			[predictionValues] = sess.run([predictions], {inputs: testInputs} )
+			testAccuracy       = accuracy(testLabels, predictionValues)
+			testPrecision      = precision(testLabels, predictionValues)
+			testRecall         = recall(testLabels, predictionValues)
+			testF1             = f1(testLabels, predictionValues)
+			print("\t   accuracy={0}, precision={1}, recall={2}, f1={3}".format(
+				testAccuracy, testPrecision, testRecall, testF1))
+			print()
+			if testAccuracy >= bestAccuracy: bestAccuracy = testAccuracy
+			else:                        pass #break
+
+def lda(trainInputs, trainLabels, testInputs, testLabels):
+	ldaModel = LinearDiscriminantAnalysis()
+	ldaModel.fit(trainInputs, trainLabels)
+	ldaPredictions = ldaModel.predict(testInputs)
+	ldaAccuracy    = accuracy(ldaPredictions, testLabels) #np.mean( testingLabels == ldaPredictions)
+	ldaPrecision   = precision(ldaPredictions, testLabels)
+	ldaRecall      = recall(ldaPredictions, testLabels)
+	ldaF1          = f1(ldaPredictions, testLabels)
+	print("\t   accuracy={0}, precision={1}, recall={2}, f1={3}".format(
+		ldaAccuracy, ldaPrecision, ldaRecall, ldaF1))
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Detect ERPs in EEG")
+	parser.add_argument("--data", metavar="data.json", required=True, help="Path to EDF-style JSON file")
 	parser.add_argument("--regression", action="store_true", help="Train logistic regression model")
 	parser.add_argument("--lda", action="store_true", help="Train linear discriminant model")
 	args = parser.parse_args()
 
 	main(args)
-
